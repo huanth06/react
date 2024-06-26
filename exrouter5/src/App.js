3d88c51ff5fb799e6de93b1a6bd4c1d947e5b917
Huan <39738858+huanth06@users.noreply.github.com>
2024-06-26 11:20:37 +0700

    update

--- exrouter5/src/App.js
+++ exrouter5/src/App.js
@@ -1,17 +1,7 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Home from "./pages/home/Home";
-import Admin from "./pages/admin/Admin";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import ProductList from "./pages/home/products/ProductList";
-import ListOrder from "./pages/home/ListOrder";
-import Student from "./pages/home/Student";
-import AddStudent from "./pages/home/AddStudent";
-import DetailStudent from "./pages/home/DetailStudent";
-import ProductDetail from "./pages/home/products/ProductDetail";
-import ProductAdd from "./pages/home/products/ProductAdd";
-import ProudctEdit from "./pages/home/products/ProudctEdit";
 import BlogList from "./pages/home/posts/BlogList";
 import BlogDetail from "./pages/home/posts/BlogDetail";
 import BlogEdit from "./pages/home/posts/BlogEdit";
